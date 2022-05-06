d149437c71d4559f780ab5839d43fddef2045b16
Cosmin2410 <cosminnae00@yahoo.com>
2022-05-06 12:00:22 +0300

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <h1>Website</h1>
+
+      <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
